7b39a9a6d397a5a84efbe1594f16e747beff5c6f
Moulham Hallak <mou990@gmail.com>
2021-05-11 23:29:23 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -21,3 +21,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
   })
 });
+
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    let inputTag = document.querySelector('input#new-task-description');
+    let inputValue = inputTag.value;
+
+
+    let newLi = document.createElement('li');
+    newLi.innerText = inputValue;
+
+    let ulTag = document.querySelector('ul#tasks');
+    ulTag.append(newLi);
+    inputTag.value = '';
+
+  })
+});
