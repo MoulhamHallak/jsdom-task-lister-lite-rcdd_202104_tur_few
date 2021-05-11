34ab091171bf65ee81a940056ad86b08885acace
Moulham Hallak <mou990@gmail.com>
2021-05-11 23:27:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.getElementById("submitButtom").addEventListener("click", function(event) {
-    event.preventDefault();
-  });
-    const form = document.querySelector('form');
+  const form = document.querySelector('form');
 
   form.addEventListener('submit', function(event){
     event.preventDefault();
@@ -20,24 +17,4 @@ document.addEventListener("DOMContentLoaded", () => {
     inputTag.value = '';
 
   })
-});
-  const inputValue = document.getElementById("new-task-description");
-  const output = document.getElementById("tasks");
-  let listItems = [];
-  document.getElementById("submitButton").addEventListener("click", addItem){
-    function appendListItem (inputValue) {
-      let listItem = document.createElement("li");
-      listItem.textContent = inputValue;
-      listItems.appendChild(listItem);  
-    };
-
-    function addItem () {
-      const inputEl = inputValue;
-      const val = inputEl.value.trim();
-      listItems.push(val);
-      appendListItem(output, val);
-      inputEl.value = "";
-    };
-  }
-  
 });
