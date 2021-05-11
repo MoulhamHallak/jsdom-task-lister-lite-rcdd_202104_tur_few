59e7923fa8d16ebb7209341165a31902d0dc694a
Moulham Hallak <mou990@gmail.com>
2021-05-11 23:29:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   })
 });
+  const form = document.querySelector('form');
 
   form.addEventListener('submit', function(event){
     event.preventDefault();
